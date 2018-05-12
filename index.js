b185fa75383496677fa4042ce9120b575f13c164
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-12 21:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState
+  }
+  this.veto = function() { return "NO!" }  
+}
