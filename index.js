eac0876143f769db5a644b309fad824cdb268b8a
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-12 21:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ class President {
     this.yearsInOffice = yearsInOffice;
     this.homeState = homeState
     this.veto = function() { return "NO!" }
+    this.passBill = function() {return "You can do that!" }
+    this.doCharity = function() { return "I like to help people."}
+    this.conductPressInterview = function() {  return "I am proud to be an American."}
+    this.sayHi = function() { return "Hi, my name is Lyndon B Johnson. I am from Texas. I represent the Democrats, and was in office 1963-1969." }
   }
-  
 }
